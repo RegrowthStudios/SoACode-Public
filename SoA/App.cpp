5c4aa561a6e195e19ff5f0c27e6a4759de1005bb
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2014-09-23 18:18:41 -0500

    Updated for style

--- SoA/App.cpp
+++ SoA/App.cpp
@@ -1,14 +1,19 @@
 #include "stdafx.h"
-#include "App.h"
 
+#include "App.h"
 #include "ScreenList.h"
 #include "TestScreen.h"
+
 
 void App::addScreens() {
     _screenList->addScreen(new TestScreen);
     _screenList->setScreen(0);
 }
+
+
 void App::onInit() {
 }
+
+
 void App::onExit() {
 }
