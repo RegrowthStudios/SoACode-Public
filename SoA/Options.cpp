f8eb552508495f92c71f194c2326bf2981b51c37
barnold1953 <brb555@utk.edu>
2014-10-13 18:05:26 -0400

    Meaningless commit

--- SoA/Options.cpp
+++ SoA/Options.cpp
@@ -42,7 +42,7 @@ void initializeOptions() {
     graphicsOptions.motionBlur = 8;
     graphicsOptions.msaa = 0;
     graphicsOptions.maxMsaa = 32;
-    graphicsOptions.voxelLODThreshold = 256.0f;
+    graphicsOptions.voxelLODThreshold = 128.0f;
     graphicsOptions.voxelLODThreshold2 = graphicsOptions.voxelLODThreshold * graphicsOptions.voxelLODThreshold;
 
     gameOptions.invertMouse = 0;
