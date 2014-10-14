68466580be3a5832714ad4cca8d829cd6924a236
barnold1953 <brb555@utk.edu>
2014-10-14 18:09:04 -0400

    Added hacky crap

--- SoA/LoadBlockDataTask.h
+++ SoA/LoadBlockDataTask.h
@@ -2,8 +2,10 @@
 #include "LoadMonitor.h"
 #include "BlockData.h"
 #include "FileSystem.h"
+#include "GameManager.h"
+#include "Player.h"
 
-// Sample Dependency Task
+// This is hacky and temporary, it does way to much
 class LoadBlockDataTask : public ILoadTask {
     virtual void load() {
         initConnectedTextures();
@@ -15,5 +17,31 @@ class LoadBlockDataTask : public ILoadTask {
         LoadTextures();
 
         SetBlockAvgTexColors();
+
+        // Hacky shit.
+        GameManager::player = new Player();
+        Player* player = GameManager::player;
+
+#define SPEEDMOD 0.0095f
+        player->setMoveSpeed(SPEEDMOD, 0.166f);
+
+        // Initialize objects
+        for (size_t i = 1; i < Blocks.size(); i++){
+            if (Blocks[i].active && !(i >= LOWWATER && i < FULLWATER || i > FULLWATER)){
+                if (ObjectList[i] != NULL){
+                    printf("Object ID %d already taken by %s. Requested by %s.\n", i, ObjectList[i]->name.c_str(), Blocks[i].name.c_str());
+                    int a;
+                    cin >> a;
+                    exit(198);
+                }
+                ObjectList[i] = new Item(i, 1, Blocks[i].name, ITEM_BLOCK, Blocks[i].weight, 0, Blocks[i].value);
+                player->inventory.push_back(new Item(i, 9999999, Blocks[i].name, ITEM_BLOCK, Blocks[i].weight, 0, Blocks[i].value));
+            }
+        }
+
+        //for use in pressure explosions
+        Blocks[VISITED_NODE] = Blocks[NONE];
+        Blocks[VISITED_NODE].ID = VISITED_NODE;
+        Blocks[VISITED_NODE].name = "Visited Node";
     }
 };
---
+++ SoA/LoadPlanetTask.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "LoadMonitor.h"
+#include "GameManager.h"
+#include "Camera.h"
+#include "Planet.h"
+#include "OpenglManager.h"
+
+class LoadPlanetTask : public ILoadTask {
+    virtual void load() {
+        GameManager::loadPlanet("Worlds/Aldrin/");
+
+        mainMenuCamera.setPosition(glm::dvec3(0.0, 0.0, 1000000000));
+        mainMenuCamera.setDirection(glm::vec3(0.0, 0.0, -1.0));
+        mainMenuCamera.setRight(glm::vec3(cos(GameManager::planet->axialZTilt), sin(GameManager::planet->axialZTilt), 0.0));
+        mainMenuCamera.setUp(glm::cross(mainMenuCamera.right(), mainMenuCamera.direction()));
+        mainMenuCamera.setClippingPlane(1000000.0f, 30000000.0f);
+
+        GameManager::initializePlanet(mainMenuCamera.position());
+
+        mainMenuCamera.zoomTo(glm::dvec3(0.0, 0.0, GameManager::planet->radius * 1.35), 3.0, glm::dvec3(0.0, 0.0, -1.0), glm::dvec3(cos(GameManager::planet->axialZTilt), sin(GameManager::planet->axialZTilt), 0.0), glm::dvec3(0.0), GameManager::planet->radius, 0.0);
+        
+    }
+};
