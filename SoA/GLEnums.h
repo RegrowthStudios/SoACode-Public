84a6f676d786c69183815455027b3d971f91b231
Cristian Zaloj <czaloj@gmail.com>
2014-11-06 11:52:16 -0500

    Added None And VGEnum

--- SoA/GLEnums.h
+++ SoA/GLEnums.h
@@ -1,10 +1,11 @@
 #pragma once
 
-// TODO: These All Must Uppercased (ex. BufferTarget)
+typedef GLenum VGEnum;
 namespace vorb {
     namespace core {
         namespace graphics {
             enum class BufferTarget {
+                NONE = 0,
                 ARRAY_BUFFER = GL_ARRAY_BUFFER,
                 ATOMIC_COUNTER_BUFFER = GL_ATOMIC_COUNTER_BUFFER,
                 COPY_READ_BUFFER = GL_COPY_READ_BUFFER,
@@ -21,6 +22,7 @@ namespace vorb {
                 UNIFORM_BUFFER = GL_UNIFORM_BUFFER
             };
             enum class BufferUsageHint {
+                NONE = 0,
                 DYNAMIC_COPY = GL_DYNAMIC_COPY,
                 DYNAMIC_DRAW = GL_DYNAMIC_DRAW,
                 DYNAMIC_READ = GL_DYNAMIC_READ,
@@ -32,6 +34,7 @@ namespace vorb {
                 STREAM_READ = GL_STREAM_READ
             };
             enum class VertexAttribPointerType {
+                NONE = 0,
                 BYTE = GL_BYTE,
                 DOUBLE = GL_DOUBLE,
                 FIXED = GL_FIXED,
@@ -45,7 +48,166 @@ namespace vorb {
                 UNSIGNED_INT_2_10_10_10_REV = GL_UNSIGNED_INT_2_10_10_10_REV,
                 UNSIGNED_SHORT = GL_UNSIGNED_SHORT
             };
+
+            enum class TextureInternalFormat {
+                NONE = 0,
+                ALPHA = GL_ALPHA,
+                COMPRESSED_ALPHA = GL_COMPRESSED_ALPHA,
+                COMPRESSED_INTENSITY = GL_COMPRESSED_INTENSITY,
+                COMPRESSED_LUMINANCE = GL_COMPRESSED_LUMINANCE,
+                COMPRESSED_LUMINANCE_ALPHA = GL_COMPRESSED_LUMINANCE_ALPHA,
+                COMPRESSED_RED = GL_COMPRESSED_RED,
+                COMPRESSED_RED_RGTC1 = GL_COMPRESSED_RED_RGTC1,
+                COMPRESSED_RG = GL_COMPRESSED_RG,
+                COMPRESSED_RGB = GL_COMPRESSED_RGB,
+                COMPRESSED_RGBA = GL_COMPRESSED_RGBA,
+                COMPRESSED_RGBA_BPTC_UNORM = GL_COMPRESSED_RGBA_BPTC_UNORM,
+                COMPRESSED_RGB_BPTC_SIGNED_FLOAT = GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT,
+                COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT = GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT,
+                COMPRESSED_RG_RGTC2 = GL_COMPRESSED_RG_RGTC2,
+                COMPRESSED_SIGNED_RED_RGTC1 = GL_COMPRESSED_SIGNED_RED_RGTC1,
+                COMPRESSED_SIGNED_RG_RGTC2 = GL_COMPRESSED_SIGNED_RG_RGTC2,
+                COMPRESSED_SLUMINANCE = GL_COMPRESSED_SLUMINANCE,
+                COMPRESSED_SLUMINANCE_ALPHA = GL_COMPRESSED_SLUMINANCE_ALPHA,
+                COMPRESSED_SRGB = GL_COMPRESSED_SRGB,
+                COMPRESSED_SRGB_ALPHA = GL_COMPRESSED_SRGB_ALPHA,
+                DEPTH24_STENCIL8 = GL_DEPTH24_STENCIL8,
+                DEPTH32F_STENCIL8 = GL_DEPTH32F_STENCIL8,
+                DEPTH_COMPONENT = GL_DEPTH_COMPONENT,
+                DEPTH_COMPONENT16 = GL_DEPTH_COMPONENT16,
+                DEPTH_COMPONENT24 = GL_DEPTH_COMPONENT24,
+                DEPTH_COMPONENT32 = GL_DEPTH_COMPONENT32,
+                DEPTH_COMPONENT32F = GL_DEPTH_COMPONENT32F,
+                DEPTH_STENCIL = GL_DEPTH_STENCIL,
+                FLOAT_32_UNSIGNED_INT_24_8_REV = GL_FLOAT_32_UNSIGNED_INT_24_8_REV,
+                LUMINANCE = GL_LUMINANCE,
+                LUMINANCE_ALPHA = GL_LUMINANCE_ALPHA,
+                R11F_G11F_B10F = GL_R11F_G11F_B10F,
+                R16 = GL_R16,
+                R16F = GL_R16F,
+                R16I = GL_R16I,
+                R16_SNORM = GL_R16_SNORM,
+                R16UI = GL_R16UI,
+                R32F = GL_R32F,
+                R32I = GL_R32I,
+                R32UI = GL_R32UI,
+                R3_G3_B2 = GL_R3_G3_B2,
+                R8 = GL_R8,
+                R8I = GL_R8I,
+                R8_SNORM = GL_R8_SNORM,
+                R8UI = GL_R8UI,
+                RG16 = GL_RG16,
+                RG16F = GL_RG16F,
+                RG16I = GL_RG16I,
+                RG16_SNORM = GL_RG16_SNORM,
+                RG16UI = GL_RG16UI,
+                RG32F = GL_RG32F,
+                RG32I = GL_RG32I,
+                RG32UI = GL_RG32UI,
+                RG8 = GL_RG8,
+                RG8I = GL_RG8I,
+                RG8_SNORM = GL_RG8_SNORM,
+                RG8UI = GL_RG8UI,
+                RGB = GL_RGB,
+                RGB10 = GL_RGB10,
+                RGB10_A2 = GL_RGB10_A2,
+                RGB10_A2UI = GL_RGB10_A2UI,
+                RGB12 = GL_RGB12,
+                RGB16 = GL_RGB16,
+                RGB16F = GL_RGB16F,
+                RGB16I = GL_RGB16I,
+                RGB16_SNORM = GL_RGB16_SNORM,
+                RGB16UI = GL_RGB16UI,
+                RGB32F = GL_RGB32F,
+                RGB32I = GL_RGB32I,
+                RGB32UI = GL_RGB32UI,
+                RGB4 = GL_RGB4,
+                RGB5 = GL_RGB5,
+                RGB5_A1 = GL_RGB5_A1,
+                RGB8 = GL_RGB8,
+                RGB8I = GL_RGB8I,
+                RGB8_SNORM = GL_RGB8_SNORM,
+                RGB8UI = GL_RGB8UI,
+                RGB9_E5 = GL_RGB9_E5,
+                RGBA = GL_RGBA,
+                RGBA12 = GL_RGBA12,
+                RGBA16 = GL_RGBA16,
+                RGBA16F = GL_RGBA16F,
+                RGBA16I = GL_RGBA16I,
+                RGBA16_SNORM = GL_RGBA16_SNORM,
+                RGBA16UI = GL_RGBA16UI,
+                RGBA2 = GL_RGBA2,
+                RGBA32F = GL_RGBA32F,
+                RGBA32I = GL_RGBA32I,
+                RGBA32UI = GL_RGBA32UI,
+                RGBA4 = GL_RGBA4,
+                RGBA8 = GL_RGBA8,
+                RGBA8I = GL_RGBA8I,
+                RGBA8_SNORM = GL_RGBA8_SNORM,
+                RGBA8UI = GL_RGBA8UI,
+                SLUMINANCE = GL_SLUMINANCE,
+                SLUMINANCE8 = GL_SLUMINANCE8,
+                SLUMINANCE8_ALPHA8 = GL_SLUMINANCE8_ALPHA8,
+                SLUMINANCE_ALPHA = GL_SLUMINANCE_ALPHA,
+                SRGB = GL_SRGB,
+                SRGB8 = GL_SRGB8,
+                SRGB8_ALPHA8 = GL_SRGB8_ALPHA8,
+                SRGB_ALPHA = GL_SRGB_ALPHA,
+                ONE = GL_ONE,
+                TWO = 2,
+                THREE = 3,
+                FOUR = 4
+            };
+            enum class TextureFormat {
+                NONE = 0,
+                ALPHA = GL_ALPHA,
+                ALPHA_INTEGER = GL_ALPHA_INTEGER,
+                BGR = GL_BGR,
+                BGRA = GL_BGRA,
+                BGRA_INTEGER = GL_BGRA_INTEGER,
+                BGR_INTEGER = GL_BGR_INTEGER,
+                BLUE = GL_BLUE,
+                BLUE_INTEGER = GL_BLUE_INTEGER,
+                COLOR_INDEX = GL_COLOR_INDEX,
+                DEPTH_COMPONENT = GL_DEPTH_COMPONENT,
+                DEPTH_STENCIL = GL_DEPTH_STENCIL,
+                GREEN = GL_GREEN,
+                GREEN_INTEGER = GL_GREEN_INTEGER,
+                LUMINANCE = GL_LUMINANCE,
+                LUMINANCE_ALPHA = GL_LUMINANCE_ALPHA,
+                RED = GL_RED,
+                RED_INTEGER = GL_RED_INTEGER,
+                RG = GL_RG,
+                RGB = GL_RGB,
+                RGBA = GL_RGBA,
+                RGBA_INTEGER = GL_RGBA_INTEGER,
+                RGB_INTEGER = GL_RGB_INTEGER,
+                RG_INTEGER = GL_RG_INTEGER,
+                STENCIL_INDEX = GL_STENCIL_INDEX,
+                UNSIGNED_INT = GL_UNSIGNED_INT,
+                UNSIGNED_SHORT = GL_UNSIGNED_SHORT
+            };
+            enum class TexturePixelType {
+                NONE = 0,
+                BYTE = GL_BYTE,
+                FLOAT = GL_FLOAT,
+                FLOAT_32_UNSIGNED_INT_24_8_REV = GL_FLOAT_32_UNSIGNED_INT_24_8_REV,
+                HALF_FLOAT = GL_HALF_FLOAT,
+                INT = GL_INT,
+                SHORT = GL_SHORT,
+                UNSIGNED_BYTE = GL_UNSIGNED_BYTE,
+                UNSIGNED_BYTE_3_3_2 = GL_UNSIGNED_BYTE_3_3_2,
+                UNSIGNED_INT = GL_UNSIGNED_INT,
+                UNSIGNED_INT_10_10_10_2 = GL_UNSIGNED_INT_10_10_10_2,
+                UNSIGNED_INT_24_8 = GL_UNSIGNED_INT_24_8,
+                UNSIGNED_INT_8_8_8_8 = GL_UNSIGNED_INT_8_8_8_8,
+                UNSIGNED_SHORT = GL_UNSIGNED_SHORT,
+                UNSIGNED_SHORT_4_4_4_4 = GL_UNSIGNED_SHORT_4_4_4_4,
+                UNSIGNED_SHORT_5_5_5_1 = GL_UNSIGNED_SHORT_5_5_5_1,
+                UNSIGNED_SHORT_5_6_5 = GL_UNSIGNED_SHORT_5_6_5
+            };
             enum class TextureTarget {
+                NONE = 0,
                 PROXY_TEXTURE_1D = GL_PROXY_TEXTURE_1D,
                 PROXY_TEXTURE_1D_ARRAY = GL_PROXY_TEXTURE_1D_ARRAY,
                 PROXY_TEXTURE_2D = GL_PROXY_TEXTURE_2D,
@@ -80,6 +242,7 @@ namespace vorb {
                 TEXTURE_RECTANGLE = GL_TEXTURE_RECTANGLE
             };
             enum class TextureParameterName {
+                NONE = 0,
                 CLAMP_TO_BORDER = GL_CLAMP_TO_BORDER,
                 CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE,
                 DEPTH_TEXTURE_MODE = GL_DEPTH_TEXTURE_MODE,
@@ -105,7 +268,9 @@ namespace vorb {
                 TEXTURE_WRAP_S = GL_TEXTURE_WRAP_S,
                 TEXTURE_WRAP_T = GL_TEXTURE_WRAP_T
             };
+            
             enum class ShaderType {
+                NONE = 0,
                 COMPUTE_SHADER = GL_COMPUTE_SHADER,
                 FRAGMENT_SHADER = GL_FRAGMENT_SHADER,
                 GEOMETRY_SHADER = GL_GEOMETRY_SHADER,
@@ -114,6 +279,7 @@ namespace vorb {
                 VERTEX_SHADER = GL_VERTEX_SHADER
             };
             enum class ShaderParameter {
+                NONE = 0,
                 COMPILE_STATUS = GL_COMPILE_STATUS,
                 DELETE_STATUS = GL_DELETE_STATUS,
                 INFO_LOG_LENGTH = GL_INFO_LOG_LENGTH,
@@ -121,6 +287,7 @@ namespace vorb {
                 SHADER_TYPE = GL_SHADER_TYPE
             };
             enum class GetProgramParameterName {
+                NONE = 0,
                 ACTIVE_ATOMIC_COUNTER_BUFFERS = GL_ACTIVE_ATOMIC_COUNTER_BUFFERS,
                 ACTIVE_ATTRIBUTE_MAX_LENGTH = GL_ACTIVE_ATTRIBUTE_MAX_LENGTH,
                 ACTIVE_ATTRIBUTES = GL_ACTIVE_ATTRIBUTES,
@@ -149,12 +316,15 @@ namespace vorb {
                 TRANSFORM_FEEDBACK_VARYINGS = GL_TRANSFORM_FEEDBACK_VARYINGS,
                 VALIDATE_STATUS = GL_VALIDATE_STATUS
             };
+            
             enum class FramebufferTarget {
+                NONE = 0,
                 DRAW_FRAMEBUFFER = GL_DRAW_FRAMEBUFFER,
                 FRAMEBUFFER = GL_FRAMEBUFFER,
                 READ_FRAMEBUFFER = GL_READ_FRAMEBUFFER
             };
             enum class FramebufferAttachment {
+                NONE = 0,
                 AUX0 = GL_AUX0,
                 AUX1 = GL_AUX1,
                 AUX2 = GL_AUX2,
@@ -187,9 +357,11 @@ namespace vorb {
                 STENCIL_ATTACHMENT = GL_STENCIL_ATTACHMENT
             };
             enum class RenderbufferTarget {
+                NONE = 0,
                 RENDERBUFFER = GL_RENDERBUFFER,
             };
             enum class RenderbufferStorage {
+                NONE = 0,
                 DEPTH24_STENCIL8 = GL_DEPTH24_STENCIL8,
                 DEPTH32F_STENCIL8 = GL_DEPTH32F_STENCIL8,
                 DEPTH_COMPONENT = GL_DEPTH_COMPONENT,
@@ -258,6 +430,7 @@ namespace vorb {
                 STENCIL_INDEX8 = GL_STENCIL_INDEX8
             };
             enum class DrawBufferMode {
+                NONE = 0,
                 BACK = GL_BACK,
                 BACK_LEFT = GL_BACK_LEFT,
                 BACK_RIGHT = GL_BACK_RIGHT,
@@ -286,6 +459,7 @@ namespace vorb {
                 RIGHT = GL_RIGHT
             };
             enum class FramebufferErrorCode {
+                NONE = 0,
                 FRAMEBUFFER_COMPLETE = GL_FRAMEBUFFER_COMPLETE,
                 FRAMEBUFFER_INCOMPLETE_ATTACHMENT = GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT,
                 FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER = GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER,
@@ -296,15 +470,17 @@ namespace vorb {
                 FRAMEBUFFER_UNDEFINED = GL_FRAMEBUFFER_UNDEFINED,
                 FRAMEBUFFER_UNSUPPORTED = GL_FRAMEBUFFER_UNSUPPORTED
             };
+            
             enum class ErrorCode {
+                NONE = GL_NO_ERROR,
                 INVALID_ENUM = GL_INVALID_ENUM,
                 INVALID_FRAMEBUFFER_OPERATION = GL_INVALID_FRAMEBUFFER_OPERATION,
                 INVALID_OPERATION = GL_INVALID_OPERATION,
                 INVALID_VALUE = GL_INVALID_VALUE,
-                NO_ERROR = GL_NO_ERROR,
                 OUT_OF_MEMORY = GL_OUT_OF_MEMORY
             };
             enum class EnableCap {
+                NONE = 0,
                 BLEND = GL_BLEND,
                 CLIP_DISTANCE0 = GL_CLIP_DISTANCE0,
                 CLIP_DISTANCE1 = GL_CLIP_DISTANCE1,
@@ -356,7 +532,9 @@ namespace vorb {
                 VERTEX_PROGRAM_POINT_SIZE = GL_VERTEX_PROGRAM_POINT_SIZE,
                 VERTEX_PROGRAM_TWO_SIDE = GL_VERTEX_PROGRAM_TWO_SIDE
             };
+            
             enum class TextureMinFilter {
+                NONE = 0,
                 LINEAR = GL_LINEAR,
                 LINEAR_MIPMAP_LINEAR = GL_LINEAR_MIPMAP_LINEAR,
                 LINEAR_MIPMAP_NEAREST = GL_LINEAR_MIPMAP_NEAREST,
@@ -365,16 +543,20 @@ namespace vorb {
                 NEAREST_MIPMAP_NEAREST = GL_NEAREST_MIPMAP_NEAREST
             };
             enum class TextureMagFilter {
+                NONE = 0,
                 LINEAR = GL_LINEAR,
                 NEAREST = GL_NEAREST
             };
             enum class TextureWrapMode {
+                NONE = 0,
                 CLAMP_BORDER = GL_CLAMP_TO_BORDER,
                 CLAMP_EDGE = GL_CLAMP_TO_EDGE,
                 REPEAT_MIRRORED = GL_MIRRORED_REPEAT,
                 REPEAT = GL_REPEAT
             };
+
             enum class DepthFunction {
+                NONE = 0,
                 ALWAYS = GL_ALWAYS,
                 EQUAL = GL_EQUAL,
                 GEQUAL = GL_GEQUAL,
@@ -384,7 +566,9 @@ namespace vorb {
                 NEVER = GL_NEVER,
                 NOTEQUAL = GL_NOTEQUAL
             };
+            
             enum class BlendEquationMode {
+                NONE = 0,
                 FUNC_ADD = GL_FUNC_ADD,
                 FUNC_REVERSE_SUBTRACT = GL_FUNC_REVERSE_SUBTRACT,
                 FUNC_SUBTRACT = GL_FUNC_SUBTRACT,
@@ -392,6 +576,7 @@ namespace vorb {
                 MIN = GL_MIN
             };
             enum class BlendingFactorSrc {
+                NONE = 0,
                 CONSTANT_ALPHA = GL_CONSTANT_ALPHA,
                 CONSTANT_COLOR = GL_CONSTANT_COLOR,
                 DST_ALPHA = GL_DST_ALPHA,
@@ -413,6 +598,7 @@ namespace vorb {
                 ZERO = GL_ZERO
             };
             enum class BlendingFactorDest {
+                NONE = 0,
                 CONSTANT_ALPHA = GL_CONSTANT_ALPHA,
                 CONSTANT_COLOR = GL_CONSTANT_COLOR,
                 DST_ALPHA = GL_DST_ALPHA,
@@ -433,7 +619,9 @@ namespace vorb {
                 SRC_COLOR = GL_SRC_COLOR,
                 ZERO = GL_ZERO
             };
+            
             enum class PrimitiveType {
+                NONE = 0,
                 LINE_LOOP = GL_LINE_LOOP,
                 LINES = GL_LINES,
                 LINES_ADJACENCY = GL_LINES_ADJACENCY,
