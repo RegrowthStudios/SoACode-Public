aa8a1db096c4089d80eadd69892821b97afdf49f
Cristian Zaloj <czaloj@gmail.com>
2014-11-07 13:52:48 -0500

    Document Stage

--- SoA/NightVisionRenderStage.cpp
+++ SoA/NightVisionRenderStage.cpp
@@ -14,6 +14,7 @@ NightVisionRenderStage::NightVisionRenderStage(vg::GLProgram* glProgram, vg::Ful
     _texNoise.width = NIGHT_VISION_NOISE_QUALITY;
     _texNoise.height = NIGHT_VISION_NOISE_QUALITY;
 
+    // Generate random data
     i32 pixCount = _texNoise.width * _texNoise.height;
     ui8* data = new ui8[pixCount];
     Random r(clock());
@@ -21,6 +22,7 @@ NightVisionRenderStage::NightVisionRenderStage(vg::GLProgram* glProgram, vg::Ful
         data[i] = (ui8)(r.genMT() * 255.0f);
     }
 
+    // Build noise texture
     glGenTextures(1, &_texNoise.ID);
     glBindTexture(GL_TEXTURE_2D, _texNoise.ID);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, _texNoise.width, _texNoise.height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
@@ -34,7 +36,7 @@ NightVisionRenderStage::~NightVisionRenderStage() {
 }
 
 void NightVisionRenderStage::draw() {
-    _et += 0.016667f;
+    _et += DEFAULT_NOISE_TIME_STEP;
 
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, _texNoise.ID);
@@ -43,8 +45,8 @@ void NightVisionRenderStage::draw() {
     _glProgram->enableVertexAttribArrays();
     glUniform1i(_glProgram->getUniform("sceneBuffer"), 0);
     glUniform1i(_glProgram->getUniform("noiseTex"), 1);
-    glUniform1f(_glProgram->getUniform("luminanceThreshold"), 0.15f);
-    glUniform1f(_glProgram->getUniform("colorAmplification"), 5.0f);
+    glUniform1f(_glProgram->getUniform("luminanceThreshold"), DEFAULT_LUMINANCE_THRESHOLD);
+    glUniform1f(_glProgram->getUniform("colorAmplification"), DEFAULT_COLOR_AMPLIFICATION);
     glUniform1f(_glProgram->getUniform("elapsedTime"), _et);
 
     glDisable(GL_DEPTH_TEST);
--- SoA/NightVisionRenderStage.h
+++ SoA/NightVisionRenderStage.h
@@ -16,18 +16,23 @@
 #define NightVisionRenderStage_h__
 
 #define NIGHT_VISION_NOISE_QUALITY 512
+#define DEFAULT_LUMINANCE_THRESHOLD 0.15f
+#define DEFAULT_COLOR_AMPLIFICATION 5.0f
+#define DEFAULT_NOISE_TIME_STEP 0.016667f
 
 #include "FullQuadVBO.h"
 #include "GLProgram.h"
 #include "IRenderStage.h"
 #include "Texture.h"
 
+/// Renders a night vision post-process effect
 class NightVisionRenderStage : public vg::IRenderStage {
 public:
     /// Constructor which injects dependencies
     /// @param glProgram: The program used to render HDR
     /// @param quad: Quad used for rendering to screen
     NightVisionRenderStage(vg::GLProgram* glProgram, vg::FullQuadVBO* quad);
+    /// Dispose OpenGL resources
     virtual ~NightVisionRenderStage();
 
     /// Draws the render stage
@@ -35,8 +40,8 @@ public:
 private:
     vg::GLProgram* _glProgram; ///< Stores the program we use to render
     vg::FullQuadVBO* _quad; ///< For use in processing through data
-    vg::Texture _texNoise;
-    float _et = 0.0f;
+    vg::Texture _texNoise; ///< A noise texture for blurry static
+    float _et = 0.0f; ///< Counter for elapsed total time
 };
 
 #endif // NightVisionRenderStage_h__
