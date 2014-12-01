dd224d87184611a727222ed3b5f7804dc04f4374
Cristian Zaloj <czaloj@gmail.com>
2014-11-30 21:40:03 -0500

    Reordering files

---
+++ SoA/MultipleComponentSet.h
@@ -0,0 +1,27 @@
+///
+/// MultipleComponentSet.h
+/// Vorb Engine
+///
+/// Created by Cristian Zaloj on 30 Nov 2014
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// 
+///
+
+#pragma once
+
+#ifndef MultipleComponentSet_h__
+#define MultipleComponentSet_h__
+
+namespace vorb {
+    namespace core {
+        class MultipleComponentSet {
+
+        };
+    }
+}
+namespace vcore = vorb::core;
+
+#endif // MultipleComponentSet_h__
