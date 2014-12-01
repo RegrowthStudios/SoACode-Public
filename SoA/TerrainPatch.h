9a0d9704e4d61a9caeb4f3b57294900964a6d940
barnold1953 <brb555@utk.edu>
2014-12-01 12:54:00 -0500

    deleted terrain patch implementation

--- SoA/TerrainPatch.h
+++ SoA/TerrainPatch.h
@@ -15,8 +15,6 @@ extern int lodDistanceLevels[DetailLevels];
 extern int WaterIndexMap[(maxVertexWidth + 3)*(maxVertexWidth + 3) * 2];
 extern int MakeWaterQuadMap[(maxVertexWidth + 3)*(maxVertexWidth + 3)];
 
-extern vector<TerrainVertex> tvboVerts; //this is bigger that it needs to be but whatever
-extern vector<GLushort> lodIndices;
 
 class Camera;
 
@@ -55,60 +53,5 @@ public:
 
 class TerrainPatch{
 public:
-    bool debug;
-    int X, Y, Z;
-    int drawX, drawY, drawZ;
-    int cX, cY, cZ;
-    int worldX, worldY, worldZ;
-    int radius;
-    int scaledRadius;
-    int face;
-    int step;
-    int lodMapStep;
-    int lodMapSize;
-    short detailLevel;
-    bool hasSkirt;
-    bool hasProperDist;
-    bool hasBoundingBox;
-    int vecIndex;
-    int updateVecIndex;
-    int distance;
-    int width;
-    float cullRadius;
-    bool drawTrees;
-
-    HeightData *lodMap;
-    TerrainPatch* lods[4];
-    TerrainPatch *parent;
-    TerrainMesh *terrainBuffers;
-    volatile bool hasBuffers;
-    //0 = no update needed. 1 = update needed 2 = child deletion needed 
-    int updateCode;
-    int childNum;
-
-    TerrainPatch(int lodWidth);
-    ~TerrainPatch();
-
-    void NullifyBuffers();
-    void ClearBuffers();
-    void ClearTreeBuffers();
-    void ClearLODMap();
-
-    void DeleteChildren();
-    //itinializes the LOD and computes distance
-    void Initialize(int x, int y, int z, int wx, int wy, int wz, int Radius, int Face, TerrainPatch *Parent = NULL, int ChildNum = -1, int initialDetail = -1);
-    static void Draw(TerrainMesh *tb, const Camera* camera, const glm::dvec3 &PlayerPos, const glm::dvec3 &rotPlayerPos, const glm::mat4 &VP, GLuint mvpID, GLuint worldOffsetID, bool onPlanet);
-    static void DrawTrees(TerrainMesh *tb, const vg::GLProgram* program, const glm::dvec3 &PlayerPos, const glm::mat4 &VP);
-    static bool CheckHorizon(const glm::dvec3 &PlayerPoss, const glm::dvec3 &ClosestPoint);
-    //inline void ExtractChildData();
-    bool CreateMesh();
-    
-    inline void CreateChildren(int wx, int wy, int wz, int initialDetail = -1);
-    int update(int wx, int wy, int wz);
-    void CalculateDetailLevel(double dist, int threshold);
-    void SortChildren();
-
-    glm::vec3 worldNormal;
-    glm::vec3 boundingBox;
-    glm::dvec3 closestPoint;
+ 
 };
