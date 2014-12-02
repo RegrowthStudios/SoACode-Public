43d6e162ffe05423f1152d6115b99963507f4e3f
czaloj <czaloj@gmail.com>
2014-12-02 09:33:40 -0500

    Better Example

--- UnitTests/Example.cpp
+++ UnitTests/Example.cpp
@@ -1,14 +1,16 @@
-#include <map>
+#include "stdafx.h"
+#include "macros.h"
+
+#undef UNIT_TEST_BATCH
+#define UNIT_TEST_BATCH Example_
 
 TEST(MyTestGood) {
-    puts("Use a map");
     std::map<int, int> m;
     m[1] = 1;
     return true;
 }
 
 TEST(MyTestBad) {
-    puts("Bad Pointer");
     int* m = nullptr;
     *m = 0;
     return true;
