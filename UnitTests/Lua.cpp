146b5ac2074224bd387c7730043790e0f2d9b15b
czaloj <czaloj@gmail.com>
2014-12-02 13:03:30 -0500

    C++ hook test

--- UnitTests/Lua.cpp
+++ UnitTests/Lua.cpp
@@ -8,7 +8,21 @@
 
 TEST(Create) {
     using namespace OOLUA;
-    Script s;
-    s.run_chunk("print(\"Hello World\")");
+    Script vm;
+    vm.run_chunk("print(\"Hello World\")");
+    return true;
+}
+
+void printer() {
+    puts("C++ Hello World");
+}
+
+OOLUA_CFUNC(printer, lua_printer);
+
+TEST(Use) {
+    using namespace OOLUA;
+    Script vm;
+    set_global(vm, "printer", lua_printer);
+    vm.run_chunk("printer()");
     return true;
 }
