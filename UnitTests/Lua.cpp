8e2e1f5aa44e3cad14c62dbd1272c1bfbc700ffc
czaloj <czaloj@gmail.com>
2014-12-02 18:13:23 -0500

    More test stuff

--- UnitTests/Lua.cpp
+++ UnitTests/Lua.cpp
@@ -2,6 +2,9 @@
 #include "macros.h"
 
 #include <lua/oolua.h>
+extern "C" {
+    #include <lua\lua\lua.h>
+}
 
 #undef UNIT_TEST_BATCH
 #define UNIT_TEST_BATCH Lua_
@@ -13,8 +16,8 @@ TEST(Create) {
     return true;
 }
 
-void printer() {
-    puts("C++ Hello World");
+int printer(int i) {
+    return i;
 }
 
 OOLUA_CFUNC(printer, lua_printer);
@@ -23,6 +26,17 @@ TEST(Use) {
     using namespace OOLUA;
     Script vm;
     set_global(vm, "printer", lua_printer);
-    vm.run_chunk("printer()");
+    vm.load_chunk(R"(
+local a = 0
+for i = 1, 100 do
+    a = a + printer(i)
+end
+print(a)
+)");
+    lua_State* ls = vm;
+    for (int i = 0; i < 100;i++) {
+        lua_call(vm, 0, LUA_MULTRET);
+    }
+
     return true;
 }
