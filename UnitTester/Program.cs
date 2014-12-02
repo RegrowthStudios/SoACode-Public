6d359b6ac092f2744f90c12dc348d758166defcc
czaloj <czaloj@gmail.com>
2014-12-02 09:34:53 -0500

    Timer info
    Remove warnings

--- UnitTester/Program.cs
+++ UnitTester/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using UnitTests;
 
 namespace UnitTester {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Beginning Tests\n===============\n");
+            Console.WriteLine("Tests Start\n===========\n");
 
             // Run all the tests
+            Stopwatch timer = new Stopwatch();
             var tests = Tests.getTests();
             foreach(String test in tests) {
                 // Print friendly name
@@ -19,8 +21,12 @@ namespace UnitTester {
                 Console.WriteLine("Running: {0}", friendly);
 
                 // Run test
+                timer.Reset();
+                timer.Start();
                 bool result = Tests.runTest(test);
-                Console.WriteLine("Result:  {0}\n", result ? "Succeeded" : "Failed");
+                timer.Stop();
+                Console.WriteLine("Result:  {0}", result ? "Pass" : "Fail");
+                Console.WriteLine("Time:    {0} ms\n", timer.ElapsedMilliseconds);
             }
 
             Console.WriteLine("\n===========\nTests Ended");
