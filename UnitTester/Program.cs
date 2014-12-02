41e2149399b4817a93728fb8d713b9df32133d5a
czaloj <czaloj@gmail.com>
2014-12-01 22:00:50 -0500

    Unit Tests

---
+++ UnitTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests;
+
+namespace UnitTester {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Beginning Tests\n===============\n");
+
+            // Run all the tests
+            var tests = Tests.getTests();
+            foreach(String test in tests) {
+                // Print friendly name
+                String[] batches = Tests.getBatches(test);
+                String friendly = String.Join("::", batches);
+                Console.WriteLine("Running: {0}", friendly);
+
+                // Run test
+                bool result = Tests.runTest(test);
+                Console.WriteLine("Result:  {0}\n", result ? "Succeeded" : "Failed");
+            }
+
+            Console.WriteLine("\n===========\nTests Ended");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnitTests/Stdafx.h
@@ -0,0 +1,13 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently,
+// but are changed infrequently
+
+#pragma once
+
+#ifndef Stdafx_h__
+#define Stdafx_h__
+
+#include <cstdio>
+
+#endif // Stdafx_h__
+
