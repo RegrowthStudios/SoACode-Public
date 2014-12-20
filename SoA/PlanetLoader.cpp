36368834a71f4637227dcc2cf99b1cfdbaba86cb
barnold1953 <brb555@utk.edu>
2014-12-19 20:16:56 -0500

    renamed to TerrainFuncKegProperties

--- SoA/PlanetLoader.cpp
+++ SoA/PlanetLoader.cpp
@@ -9,7 +9,7 @@ KEG_ENUM_INIT_BEGIN(TerrainFunction, TerrainFunction, e);
 e->addValue("ridgedNoise", TerrainFunction::RIDGED_NOISE);
 KEG_ENUM_INIT_END
 
-class GenerationKegProperties {
+class TerrainFuncKegProperties {
 public:
     TerrainFunction func;
     int octaves = 1;
@@ -18,12 +18,12 @@ public:
     float low = 0.0f;
     float high = 0.0f;
 };
-KEG_TYPE_INIT_BEGIN_DEF_VAR(GenerationKegProperties)
-KEG_TYPE_INIT_ADD_MEMBER(GenerationKegProperties, I32, octaves);
-KEG_TYPE_INIT_ADD_MEMBER(GenerationKegProperties, F32, persistence);
-KEG_TYPE_INIT_ADD_MEMBER(GenerationKegProperties, F32, frequency);
-KEG_TYPE_INIT_ADD_MEMBER(GenerationKegProperties, F32, low);
-KEG_TYPE_INIT_ADD_MEMBER(GenerationKegProperties, F32, high);
+KEG_TYPE_INIT_BEGIN_DEF_VAR(TerrainFuncKegProperties)
+KEG_TYPE_INIT_ADD_MEMBER(TerrainFuncKegProperties, I32, octaves);
+KEG_TYPE_INIT_ADD_MEMBER(TerrainFuncKegProperties, F32, persistence);
+KEG_TYPE_INIT_ADD_MEMBER(TerrainFuncKegProperties, F32, frequency);
+KEG_TYPE_INIT_ADD_MEMBER(TerrainFuncKegProperties, F32, low);
+KEG_TYPE_INIT_ADD_MEMBER(TerrainFuncKegProperties, F32, high);
 KEG_TYPE_INIT_END
 
 PlanetLoader::PlanetLoader() {
