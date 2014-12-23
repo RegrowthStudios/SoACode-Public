456348a20b098cb6243afc53a36a71110a64b3e8
czaloj <czaloj@gmail.com>
2014-12-23 11:29:32 -0500

    Block viewing screen template

---
+++ SoA/TestBlockViewScreen.h
@@ -0,0 +1,39 @@
+///
+/// TestBlockViewScreen.h
+/// Seed of Andromeda
+///
+/// Created by Cristian Zaloj on 23 Dec 2014
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// Preview a block
+///
+
+#pragma once
+
+#ifndef TestBlockViewScreen_h__
+#define TestBlockViewScreen_h__
+
+#include <Events.hpp>
+#include <IGameScreen.h>
+
+class TestBlockView : public IGameScreen {
+public:
+    /************************************************************************/
+    /* IGameScreen functionality                                            */
+    /************************************************************************/
+    virtual i32 getNextScreen() const override;
+    virtual i32 getPreviousScreen() const override;
+    virtual void build() override;
+    virtual void destroy(const GameTime& gameTime) override;
+    virtual void onEntry(const GameTime& gameTime) override;
+    virtual void onExit(const GameTime& gameTime) override;
+    virtual void onEvent(const SDL_Event& e) override;
+    virtual void update(const GameTime& gameTime) override;
+    virtual void draw(const GameTime& gameTime) override;
+private:
+
+};
+
+#endif // TestBlockViewScreen_h__
