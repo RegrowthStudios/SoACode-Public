e158c0b9661ce31b518f906b94e626147fb038fe
czaloj <czaloj@gmail.com>
2014-12-23 22:22:41 -0500

    Load blocks from drag-drop

--- SoA/TestBlockViewScreen.cpp
+++ SoA/TestBlockViewScreen.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include "TestBlockViewScreen.h"
 
+#include <InputDispatcher.h>
+#include <IOManager.h>
+
+#include "BlockLoader.h"
+
 i32 TestBlockView::getNextScreen() const {
     return SCREEN_INDEX_NO_SCREEN;
 }
@@ -16,7 +21,16 @@ void TestBlockView::destroy(const GameTime& gameTime) {
 }
 
 void TestBlockView::onEntry(const GameTime& gameTime) {
-    // Empty
+    m_hooks.addAutoHook(&m_blocks.onBlockAddition, [&] (void* s, ui16 id) {
+        printf("Loaded Block: %s = %d\n", m_blocks[id].name.c_str(), id);
+    });
+    m_hooks.addAutoHook(&vui::InputDispatcher::window.onFile, [&] (void* s, const vui::WindowFileEvent& e) {
+        loadBlocks(e.file);
+    });
+
+    // Set clear state
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0);
 }
 void TestBlockView::onExit(const GameTime& gameTime) {
     // Empty
@@ -29,5 +43,10 @@ void TestBlockView::update(const GameTime& gameTime) {
     // Empty
 }
 void TestBlockView::draw(const GameTime& gameTime) {
-    // Empty
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void TestBlockView::loadBlocks(const cString file) {
+    IOManager iom;
+    BlockLoader::load(&iom, file, &m_blocks);
 }
