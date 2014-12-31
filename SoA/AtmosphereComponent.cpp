4e9f109dc07cfe5b958d480082cdfab82a14d753
barnold1953 <brb555@utk.edu>
2014-12-31 02:22:50 -0500

    Added atmosphereComponent

---
+++ SoA/AtmosphereComponent.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "AtmosphereComponent.h"
+
+
+AtmosphereComponent::AtmosphereComponent() {
+}
+
+AtmosphereComponent::AtmosphereComponent() {
+
+}
+
+
+AtmosphereComponent::~AtmosphereComponent() {
+}
+
+AtmosphereComponent::~AtmosphereComponent() {
+
+}
+
+void AtmosphereComponent::init() {
+
+}
+
+void AtmosphereComponent::draw() {
+
+}
+
+void AtmosphereComponent::loadProperties(const nString& filePath) {
+
+}
