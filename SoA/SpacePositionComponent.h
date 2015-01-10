43ac5930b6b20e818ebfee9709ac15a9f4cffd4a
barnold1953 <brb555@utk.edu>
2015-01-10 14:43:30 -0500

    Added SpacePositionComponent

---
+++ SoA/SpacePositionComponent.h
@@ -0,0 +1,24 @@
+///
+/// SpacePositionComponent.h
+/// Seed of Andromeda
+///
+/// Created by Benjamin Arnold on 10 Jan 2015
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// Position in space
+///
+
+#pragma once
+
+#ifndef SpacePositionComponent_h__
+#define SpacePositionComponent_h__
+
+class SpacePositionComponent {
+public:
+    f64v3 position;
+    f64q orientation;
+};
+
+#endif // SpacePositionComponent_h__
