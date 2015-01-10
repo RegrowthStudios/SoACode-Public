d51b8bc899a12c809e5dfc80cfdd82c55c1a63fd
barnold1953 <brb555@utk.edu>
2015-01-10 15:15:02 -0500

    Added AabbCollidableComponent

---
+++ SoA/AabbCollidableComponent.h
@@ -0,0 +1,23 @@
+///
+/// AabbCollidableComponent.h
+/// Seed of Andromeda
+///
+/// Created by Benjamin Arnold on 10 Jan 2015
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// Component for AABB
+///
+
+#pragma once
+
+#ifndef AabbCollidableComponent_h__
+#define AabbCollidableComponent_h__
+
+class AabbCollidableComponent {
+public:
+    f32v3 box; ///< x, y, z widths
+};
+
+#endif // AabbCollidableComponent_h__
