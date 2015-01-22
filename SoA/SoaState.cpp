f4461145c3841b7426f08532983b2bf8d33df952
barnold1953 <brb555@utk.edu>
2015-01-22 17:50:17 -0500

    Fixed black texture bug

--- SoA/SoaState.cpp
+++ SoA/SoaState.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 #include "SoaState.h"
 
-#include "GLProgramManager.h"
+#include <Vorb/io/IOManager.h>
+
 #include "DebugRenderer.h"
+#include "GLProgramManager.h"
 #include "MeshManager.h"
+#include "PlanetLoader.h"
 
 SoaState::~SoaState() {
     // Empty
