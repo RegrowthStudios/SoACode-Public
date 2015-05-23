dbe99236bfc3e015ab59692e5d87469e61e47861
barnold1953 <brb555@utk.edu>
2015-05-23 17:29:55 -0400

    Added missing files

---
+++ SoA/PlanetRingsComponentRenderer.h
@@ -0,0 +1,48 @@
+///
+/// PlanetRingsComponentRenderer.h
+/// Seed of Andromeda
+///
+/// Created by Benjamin Arnold on 23 May 2015
+/// Copyright 2014 Regrowth Studios
+/// All Rights Reserved
+///
+/// Summary:
+/// The renderer for PlanetRingsComponent
+///
+
+#pragma once
+
+#ifndef PlanetRingsComponentRenderer_h__
+#define PlanetRingsComponentRenderer_h__
+
+#include <Vorb/ecs/ECS.h>
+#include <Vorb/ecs/ComponentTable.hpp>
+#include <Vorb/VorbPreDecl.inl>
+#include <Vorb/graphics/gtypes.h>
+#include <Vorb/graphics/FullQuadVBO.h>
+
+struct SpaceLightComponent;
+struct PlanetRingsComponent;
+
+DECL_VG(class GLProgram)
+
+class PlanetRingsComponentRenderer {
+public:
+    PlanetRingsComponentRenderer();
+    ~PlanetRingsComponentRenderer();
+
+    void draw(const PlanetRingsComponent& prCmp,
+              const f32m4& VP,
+              const f32v3& relCamPos,
+              const f32v3& lightPos,
+              const f32 planetRadius,
+              const SpaceLightComponent* spComponent);
+    void disposeShader();
+private:
+
+    vg::GLProgram* m_program = nullptr;
+    vg::FullQuadVBO m_quad;
+    bool m_isInitialized = false;
+};
+
+#endif // PlanetRingsComponentRenderer_h__
