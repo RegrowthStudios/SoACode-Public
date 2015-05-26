c79d3205bbfb1ce79233c563c83d04958b04b654
barnold1953 <brb555@utk.edu>
2015-05-26 16:48:48 -0400

    Apply axis rotation to clouds

--- SoA/SpaceSystemRenderStage.cpp
+++ SoA/SpaceSystemRenderStage.cpp
@@ -179,7 +179,9 @@ void SpaceSystemRenderStage::drawBodies() {
         auto& l = lightCache[it.first];
         f32v3 lightDir(glm::normalize(l.first - *pos));
         
-        m_cloudsComponentRenderer.draw(cCmp, m_spaceCamera->getViewProjectionMatrix(), relCamPos, lightDir, l.second, m_spaceSystem->m_atmosphereCT.getFromEntity(it.first));
+        m_cloudsComponentRenderer.draw(cCmp, m_spaceCamera->getViewProjectionMatrix(), relCamPos, lightDir, l.second,
+                                       m_spaceSystem->m_axisRotationCT.getFromEntity(it.first), 
+                                       m_spaceSystem->m_atmosphereCT.getFromEntity(it.first));
     }
 
     // Render atmospheres
