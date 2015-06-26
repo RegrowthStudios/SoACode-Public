c00fa73319ace5e4a66c64b30a8dddc2db03b158
barnold1953 <brb555@utk.edu>
2015-06-26 14:27:10 -0400

    Better noise generation code

--- SoA/NoiseShaderGenerator.cpp
+++ SoA/NoiseShaderGenerator.cpp
@@ -131,58 +131,33 @@ void NoiseShaderGenerator::addNoiseFunctions(OUT nString& fSource, const nString
             }           
             nextOp = op;
         } else { // It's a noise function
-            fSource = fSource +
-                "total = 0.0;\n" +
-                "amplitude = 1.0;\n" +
-                "maxAmplitude = 0.0;\n" +
-                "frequency = " + TS(fn.frequency) + ";\n" +
-
-                "for (int i = 0; i < " + TS(fn.octaves) + "; i++) {\n";
             switch (fn.func) {
                 case TerrainStage::CUBED_NOISE:
+                    fSource += "total = cubedNoise(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
+                    break;
                 case TerrainStage::SQUARED_NOISE:
+                    fSource += "total = squaredNoise(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
+                    break;
                 case TerrainStage::NOISE:
-                    fSource += "total += snoise(pos * frequency) * amplitude;\n";
+                    fSource += "total = noise(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
                 case TerrainStage::RIDGED_NOISE:
-                    fSource += "total += ((1.0 - abs(snoise(pos * frequency))) * 2.0 - 1.0) * amplitude;\n";
+                    fSource += "total = ridgedNoise(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
                 case TerrainStage::ABS_NOISE:
-                    fSource += "total += abs(snoise(pos * frequency)) * amplitude;\n";
+                    fSource += "total = absNoise(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
                 case TerrainStage::CELLULAR_NOISE:
-                    fSource += "vec2 ff = cellular(pos * frequency);\n";
-                    fSource += "total += (ff.y - ff.x) * amplitude;\n";
+                    fSource += "total = cellularFractal(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
                 case TerrainStage::CELLULAR_SQUARED_NOISE:
-                    fSource += "vec2 ff = cellular(pos * frequency);\n";
-                    fSource += "tmp = ff.y - ff.x;\n";
-                    fSource += "total += tmp * tmp * amplitude;\n";
+                    fSource += "total = cellularSquared(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
                 case TerrainStage::CELLULAR_CUBED_NOISE:
-                    fSource += "vec2 ff = cellular(pos * frequency);\n";
-                    fSource += "tmp = ff.y - ff.x;\n";
-                    fSource += "total += tmp * tmp * tmp * amplitude;\n";
+                    fSource += "total = cellularCubed(pos, " + TS(fn.octaves) + ", " + TS(fn.frequency) + ", " + TS(fn.persistence) + ");\n";
                     break;
             }
-            fSource = fSource +
-                "  frequency *= 2.0;\n" +
-                "  maxAmplitude += amplitude;\n" +
-                "  amplitude *= " + TS(fn.persistence) + ";\n" +
-                "}\n";
 
-            fSource = fSource + "total = (total / maxAmplitude);\n";
-            // Handle any post processes per noise
-            switch (fn.func) {
-                case TerrainStage::CUBED_NOISE:
-                    fSource = fSource + "total = total * total * total;\n";
-                    break;
-                case TerrainStage::SQUARED_NOISE:
-                    fSource = fSource + "total = total * total;\n";
-                    break;
-                default:
-                    break;
-            }
             // Conditional scaling. 
             fSource += "float " + h + " = ";
             if (fn.low != -1.0f || fn.high != 1.0f) {
