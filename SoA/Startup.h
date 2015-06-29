39128800b7cedc7448d55b9a436660e1c2332fae
czaloj <czaloj@gmail.com>
2015-06-29 00:52:36 -0700

    Startup logic

---
+++ SoA/Startup.h
@@ -0,0 +1,35 @@
+//
+// Startup.h
+// Seed of Andromeda
+//
+// Created by Cristian Zaloj on 29 Jun 2015
+// Copyright 2014 Regrowth Studios
+// All Rights Reserved
+//
+// Summary:
+// 
+//
+
+#pragma once
+
+#ifndef Startup_h__
+#define Startup_h__
+
+/*! @brief Designates the startup format.
+ */
+enum class Startup {
+    APP,
+    CONSOLE,
+    HELP,
+    EXIT
+};
+
+/*! @brief Determines how to startup the application from its arguments.
+ * 
+ * @param argc: Number of process arguments.
+ * @param argv: Values of process arguments.
+ * @return The way to start the application.
+ */
+Startup startup(int argc, cString* argv);
+
+#endif // Startup_h__
