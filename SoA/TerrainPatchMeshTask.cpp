4dabbe0a721590b7185f562410a87a47c914a904
barnold1953 <brb555@utk.edu>
2015-07-01 19:10:20 -0400

    Sort of fixed far terrain generation

--- SoA/TerrainPatchMeshTask.cpp
+++ SoA/TerrainPatchMeshTask.cpp
@@ -25,43 +25,56 @@ void TerrainPatchMeshTask::execute(WorkerData* workerData) {
     f32v3 worldNormalData[PADDED_PATCH_WIDTH][PADDED_PATCH_WIDTH];
     f64v3 pos;
     f32v3 tmpPos;
-    const i32v3& coordMapping = VoxelSpaceConversions::VOXEL_TO_WORLD[(int)m_cubeFace];
-    const f32v2& coordMults = f32v2(VoxelSpaceConversions::FACE_TO_WORLD_MULTS[(int)m_cubeFace]);
-    const i32v3& trueMapping = VoxelSpaceConversions::VOXEL_TO_WORLD[(int)m_cubeFace];
 
-    const float VERT_WIDTH = m_width / (PATCH_WIDTH - 1);
-    bool isSpherical = m_mesh->getIsSpherical();
-    // Check for early delete
+    SphericalTerrainCpuGenerator* generator = m_patchData->generator;
     if (m_mesh->m_shouldDelete) {
         delete m_mesh;
         return;
     }
-    SphericalTerrainCpuGenerator* generator = m_patchData->generator;
-    m_startPos.y *= (f32)VoxelSpaceConversions::FACE_Y_MULTS[(int)m_cubeFace];
-    memset(heightData, 0, sizeof(heightData));
-    for (int z = 0; z < PADDED_PATCH_WIDTH; z++) {
-        for (int x = 0; x < PADDED_PATCH_WIDTH; x++) {
-            pos[coordMapping.x] = (m_startPos.x + (x - 1) * VERT_WIDTH) * coordMults.x;
-            pos[coordMapping.y] = m_startPos.y;
-            pos[coordMapping.z] = (m_startPos.z + (z - 1) * VERT_WIDTH) * coordMults.y;
-            f64v3 normal(glm::normalize(pos));
-            worldNormalData[z][x] = normal;
-            pos = normal * m_patchData->radius;
-            heightData[z][x][0] = (f32)generator->getHeightValue(pos) * KM_PER_M;
-            heightData[z][x][1] = (f32)generator->getTemperatureValue(pos);
-            heightData[z][x][2] = (f32)generator->getHumidityValue(pos);
-           
-            // offset position by height;
-            // TODO(Ben): Separate this out to prevent branching inside
-            if (isSpherical) {
+    const float VERT_WIDTH = m_width / (PATCH_WIDTH - 1);
+    bool isSpherical = m_mesh->getIsSpherical();
+
+    if (isSpherical) {
+        const i32v3& coordMapping = VoxelSpaceConversions::VOXEL_TO_WORLD[(int)m_cubeFace];
+        const f32v2& coordMults = f32v2(VoxelSpaceConversions::FACE_TO_WORLD_MULTS[(int)m_cubeFace]);
+      
+        m_startPos.y *= (f32)VoxelSpaceConversions::FACE_Y_MULTS[(int)m_cubeFace];
+        for (int z = 0; z < PADDED_PATCH_WIDTH; z++) {
+            for (int x = 0; x < PADDED_PATCH_WIDTH; x++) {
+                pos[coordMapping.x] = (m_startPos.x + (x - 1) * VERT_WIDTH) * coordMults.x;
+                pos[coordMapping.y] = m_startPos.y;
+                pos[coordMapping.z] = (m_startPos.z + (z - 1) * VERT_WIDTH) * coordMults.y;
+                f64v3 normal(glm::normalize(pos));
+                worldNormalData[z][x] = normal;
+                pos = normal * m_patchData->radius;
+                heightData[z][x][0] = (f32)generator->getHeightValue(pos) * KM_PER_M;
+                heightData[z][x][1] = (f32)generator->getTemperatureValue(pos);
+                heightData[z][x][2] = (f32)generator->getHumidityValue(pos);
+
+                // offset position by height;
                 positionData[z][x] = normal * (m_patchData->radius + heightData[z][x][0]);
-            } else {
-                tmpPos[trueMapping.x] = pos.x;
-                tmpPos[trueMapping.y] = m_patchData->radius * (f32)VoxelSpaceConversions::FACE_Y_MULTS[(int)m_cubeFace];
-                tmpPos[trueMapping.z] = pos.z;
-                worldNormalData[z][x] = glm::normalize(tmpPos);
-                pos.y += heightData[z][x][0];
-                positionData[z][x] = pos;
+            }
+        }
+    } else {
+        const i32v3& coordMapping = VoxelSpaceConversions::VOXEL_TO_WORLD[(int)m_cubeFace];
+        const f32v2& coordMults = f32v2(VoxelSpaceConversions::FACE_TO_WORLD_MULTS[(int)m_cubeFace]);
+
+        m_startPos.y *= (f32)VoxelSpaceConversions::FACE_Y_MULTS[(int)m_cubeFace];
+        for (int z = 0; z < PADDED_PATCH_WIDTH; z++) {
+            for (int x = 0; x < PADDED_PATCH_WIDTH; x++) {
+                f32v2 spos;
+                spos.x = (m_startPos.x + (x - 1) * VERT_WIDTH) * coordMults.x;
+                spos.y = (m_startPos.z + (z - 1) * VERT_WIDTH) * coordMults.y;
+                pos[coordMapping.x] = spos.x;
+                pos[coordMapping.y] = m_startPos.y;
+                pos[coordMapping.z] = spos.y;
+                f64v3 normal(glm::normalize(pos));
+                worldNormalData[z][x] = normal;
+                pos = normal * m_patchData->radius;
+                heightData[z][x][0] = (f32)generator->getHeightValue(pos) * KM_PER_M;
+                heightData[z][x][1] = (f32)generator->getTemperatureValue(pos);
+                heightData[z][x][2] = (f32)generator->getHumidityValue(pos);
+                positionData[z][x] = f32v3(spos.x, heightData[z][x][0], spos.y);   
             }
         }
     }
